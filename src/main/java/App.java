0215a4dd9fa08a856598b6bb91b1cafd96c35fad
Maksym Martynenko <martynenko.ma107@gmail.com>
2022-02-17 21:24:21 +0500

    spring probe

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("helloworld: " + (bean == bean2));
+        System.out.println(("cat: " + (cat1 == cat2)));
         System.out.println(bean.getMessage());
     }
 }
